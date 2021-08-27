1d740253d64662fb7b77e9b61d38dc64b2a181c6
muzzammil <muzzammil.shahid.311@gmail.com>
2021-08-27 15:16:18 +0500

    initial commit

---
+++ app/src/main/java/com/deskconn/githubactions/MainActivity.java
@@ -0,0 +1,14 @@
+package com.deskconn.githubactions;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
